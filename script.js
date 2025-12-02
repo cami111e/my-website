88e93b21d8c32132e4a1c9b9e24f54ede31f523a
Camille Wilson <158201206+cami111e@users.noreply.github.com>
2025-12-02 16:12:27 -0700

    About me, scroll to menu, animations

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const about = document.getElementById("about");
+    const arrow = document.getElementById("downArrow");
+    const menu = document.getElementById("menu");
+
+    window.addEventListener("load", () => {
+        // on load, fade in about section again and fade to top of screen
+        setTimeout(() => {
+            about.style.opacity = 1;
+        }, 50);
+        window.scrollTo(0, 0);
+    });
+
+    // scroll to menu section when arrow's clicked
+    arrow.addEventListener("click", () => {
+        menu.scrollIntoView({ behavior: "smooth" });
+    });
+});
