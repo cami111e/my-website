15183b51f078a1944b7e9407ffe4e4641282fd48
Camille Wilson <158201206+cami111e@users.noreply.github.com>
2025-12-02 16:22:08 -0700

    Attempt to fix site reload behaviour

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const arrow = document.getElementById("downArrow");
     const menu = document.getElementById("menu");
 
+    window.scrollTo(0, 0);
+
     window.addEventListener("load", () => {
         // on load, fade in about section again and fade to top of screen
         setTimeout(() => {
             about.style.opacity = 1;
         }, 50);
-        window.scrollTo(0, 0);
+        
     });
 
     // scroll to menu section when arrow's clicked
